1107712d674514b7649a39b4b27213addc83b320
AbdelrahmanDesOki <desoki.abdelrahman@gmail.com>
2022-01-12 00:41:12 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Abdelrahman  Desoki on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
